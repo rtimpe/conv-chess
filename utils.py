e53ef7cd28b1768364dbf862f26b204c17a6eb8d
Robert Timpe <rtimpe>
2017-10-15 23:44:50 -0700

    some cleanup for easier experimentation

--- utils.py
+++ utils.py
@@ -14,6 +14,23 @@ def get_boards_from_game(game):
         boards.append(node.board())
     return boards
 
+def load_boards_from_pgnf(fname, num_games=100):
+    with open(fname) as pgnf:
+        seen_boards = {}
+        for i in range(num_games):
+            game = chess.pgn.read_game(pgnf)
+            boards = get_boards_from_game(game)
+            for board in boards:
+                arr = board_to_arr(board)
+                board_fen = board.board_fen()
+                # since we're reading board positions from actual games, there
+                # will be many duplicate positions.  ignore them to keep the
+                # training data unbiased
+                if board_fen not in seen_boards:
+                    seen_boards[board_fen] = arr
+        X = np.array(list(seen_boards.values()))
+    return X
+
 def board_to_arr(board):
     color_to_num = {chess.WHITE: 1, chess.BLACK: -1}
     arr = np.zeros((8,8,6))
@@ -70,9 +87,13 @@ def create_cae(x, filter_sizes):
     num_filters = 20
 
     prev_layer = x
+    all_variables = []
     for f in filter_sizes:
         filters = tf.Variable(tf.random_uniform([3, 3, int(prev_layer.get_shape()[3]), f]))
         b = tf.Variable(tf.zeros([f]))
+        all_variables.append(filters)
+        all_variables.append(b)
+
         conv_out = tf.nn.tanh(tf.nn.conv2d(prev_layer, filters, [1,1,1,1], "SAME") + b)
 
         pool_out = tf.nn.max_pool(conv_out, [1,2,2,1], [1,2,2,1], 'SAME')
@@ -83,6 +104,9 @@ def create_cae(x, filter_sizes):
     for f in reversed([6] + filter_sizes[1:]):
         filters = tf.Variable(tf.random_uniform([3, 3, int(prev_layer.get_shape()[3]), f]))
         b = tf.Variable(tf.zeros([f]))
+        all_variables.append(filters)
+        all_variables.append(b)
+
         conv_out = tf.nn.tanh(tf.nn.conv2d(prev_layer, filters, [1,1,1,1], "SAME") + b)
 
         pool_out = UnPooling2x2ZeroFilled(conv_out)
@@ -111,10 +135,11 @@ def UnPooling2x2ZeroFilled(x):
         ret = tf.reshape(out, tf.stack([-1, shv[1] * 2, shv[2] * 2, sh[3]]))
     return ret
 
-def create(x, layer_sizes):
+def create_fully_connected(x, layer_sizes, sess):
     # Build the encoding layers
     next_layer_input = x
 
+    all_variables = []
     encoding_matrices = []
     for dim in layer_sizes:
         input_dim = int(next_layer_input.get_shape()[1])
@@ -127,6 +152,10 @@ def create(x, layer_sizes):
 
         # We are going to use tied-weights so store the W matrix for later reference.
         encoding_matrices.append(W)
+
+        # also store variables for initialization
+        all_variables.append(W)
+        all_variables.append(b)
 
         output = tf.nn.tanh(tf.matmul(next_layer_input,W) + b)
 
@@ -148,11 +177,46 @@ def create(x, layer_sizes):
         output = tf.nn.tanh(tf.matmul(next_layer_input,W) + b)
         next_layer_input = output
 
+        all_variables.append(b)
+
     # the fully encoded and reconstructed value of x is here:
     reconstructed_x = next_layer_input
 
+    # initialize variables here.  not sure if this is the best place to do it?
+    # but good enough for now
+    init = tf.variables_initializer(all_variables)
+    sess.run(init)
+
     return {
-    'encoded': encoded_x,
-    'decoded': reconstructed_x,
-    'cost' : tf.sqrt(tf.reduce_mean(tf.square(x-reconstructed_x)))
+        'encoded': encoded_x,
+        'decoded': reconstructed_x,
+        'cost' : tf.sqrt(tf.reduce_mean(tf.square(x-reconstructed_x)))
     }
+
+
+# https://stackoverflow.com/questions/35164529/in-tensorflow-is-there-any-way-to-just-initialize-uninitialised-variables#35618160
+def initialize_uninitialized(sess):
+    global_vars          = tf.global_variables()
+    is_not_initialized   = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+    not_initialized_vars = [v for (v, f) in zip(global_vars, is_not_initialized) if not f]
+
+    # print [str(i.name) for i in not_initialized_vars] # only for testing
+    if len(not_initialized_vars):
+        sess.run(tf.variables_initializer(not_initialized_vars))
+
+def train(autoencoder, X, flatten, input_ph, sess, num_iters=20000, batch_size=100):
+    train_step = tf.train.AdamOptimizer(0.001).minimize(autoencoder['cost'])
+
+    # initialize remaining variables (should just be variables from the optimizer)
+    initialize_uninitialized(sess)
+    # print(sess.run(tf.report_uninitialized_variables()))
+    # sess.run(tf.initialize_variables([tf.get_variable(name) for name in sess.run(tf.report_uninitialized_variables())]))
+
+    for i in range(num_iters):
+        inds = np.random.choice(X.shape[0], batch_size)
+        X_batch = X[inds]
+        if flatten:
+           X_batch = np.reshape(X_batch, (batch_size, -1))
+        sess.run(train_step, feed_dict={input_ph: X_batch})
+        if i % 1000 == 0:
+            print(i, " cost", sess.run(autoencoder['cost'], feed_dict={input_ph: X_batch}))
